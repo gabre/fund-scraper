fd16668bb1958344d72d72ece091ce613892bbe8
gabre <gabre@caesar.elte.hu>
2019-04-01 12:44:39 +0200

    Fix script errors

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+funds_url = 'https://www.kh.hu/megtakaritas-befektetes/befektetesi-alap-arfolyam'
 driver = None
+
+isin = 'HU0000715594'
+navigate_to_unit_cell = "//td[text()='%s']/..//td[contains(@class, 'navPerUnit')][1]"
+
 try:
     driver = webdriver.Firefox()
-    driver.get('https://www.kh.hu/megtakaritas-befektetes/befektetesi-alap-arfolyam')
+    driver.get(funds_url)
 
     element = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.XPATH,
-            "//td[text()='HU0000715594']/..//td[contains(@class, 'navPerUnit')][1]")))
+                                          navigate_to_unit_cell % isin)))
     print(element.text)
-except e:
-    print("ERROR: " + e)
+except Exception as e:
+    print("ERROR: " + str(e))
 finally:
     if driver is not None:
         driver.close()
